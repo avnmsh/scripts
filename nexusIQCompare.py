f15eff46547faddf925ce07722b3033c0181549d
avnmsh <146586158+avnmsh@users.noreply.github.com>
2023-10-05 15:45:38 +0100

    Update nexusIQCompare.py

--- nexusIQCompare.py
+++ nexusIQCompare.py
@@ -9,26 +9,26 @@ with open(current_report_path, 'r') as current_report_file, open(previous_report
     current_report = json.load(current_report_file)
     previous_report = json.load(previous_report_file)
 
-# Extract CVEs from the current and previous reports
-current_components = current_report.get('components', [])
-previous_components = previous_report.get('components', [])
-
 # Function to extract CVE IDs from a list of components
 def extract_cve_ids(components):
     cve_ids = set()
     for component in components:
-        security_data = component.get('securityData', {})
-        security_issues = security_data.get('securityIssues', [])
-        for issue in security_issues:
-            references = issue.get('reference', [])
-            for reference in references:
-                if reference.startswith("CVE-"):
-                    cve_ids.add(reference)
+        try:
+            security_data = component.get('securityData', {})
+            security_issues = security_data.get('securityIssues', [])
+            for issue in security_issues:
+                references = issue.get('reference', [])
+                for reference in references:
+                    if reference.startswith("CVE-"):
+                        cve_ids.add(reference)
+        except AttributeError:
+            # Handle cases where the structure is not as expected
+            continue
     return cve_ids
 
 # Extract CVE IDs from the current and previous reports
-current_cve_ids = extract_cve_ids(current_components)
-previous_cve_ids = extract_cve_ids(previous_components)
+current_cve_ids = extract_cve_ids(current_report.get('components', []))
+previous_cve_ids = extract_cve_ids(previous_report.get('components', []))
 
 # Find new CVEs introduced in the current report
 new_cves = current_cve_ids - previous_cve_ids
