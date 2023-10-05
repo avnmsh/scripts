0047e12d142d0b3c06f0aa55b5400fbc6c5481bd
avnmsh <146586158+avnmsh@users.noreply.github.com>
2023-10-05 21:37:35 +0100

    Update Test1.py

--- Test1.py
+++ Test1.py
@@ -8,17 +8,21 @@ with open('old_report.json', 'r') as old_file, open('new_report.json', 'r') as n
 # Extract the "reference" values from both reports
 old_references = set()
 for component in old_data.get("components", []):
-    for issue in component.get("securityData", {}).get("securityIssues", []):
-        reference = issue.get("reference")
-        if reference:
-            old_references.add(reference)
+    security_data = component.get("securityData")
+    if security_data:
+        for issue in security_data.get("securityIssues", []):
+            reference = issue.get("reference")
+            if reference:
+                old_references.add(reference)
 
 new_references = set()
 for component in new_data.get("components", []):
-    for issue in component.get("securityData", {}).get("securityIssues", []):
-        reference = issue.get("reference")
-        if reference:
-            new_references.add(reference)
+    security_data = component.get("securityData")
+    if security_data:
+        for issue in security_data.get("securityIssues", []):
+            reference = issue.get("reference")
+            if reference:
+                new_references.add(reference)
 
 # Find new references in the new report compared to the old report
 new_references_only = new_references - old_references
